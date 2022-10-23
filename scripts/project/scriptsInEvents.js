f604c2a09f20423397bf69b68b25b2db5d52b3dc
gameofart@gmail.com <gameofart@gmail.com>
2022-10-23 18:41:02 +0300

    Update scriptsInEvents.js

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -10,7 +10,7 @@ const scriptsInEvents = {
 
 	async Game_Event46_Act8(runtime, localVars)
 	{
-		report_result(0,zkp3lptsfa6ep9325t9sh5f2c9126xu4)
+		report_result(1004,zkp3lptsfa6ep9325t9sh5f2c9126xu4)
 		
 	}
 
